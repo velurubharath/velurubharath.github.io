64e9f97b1ff4c4c7bccbcf839199e33458038690
velurubharath <velurubharath@gmail.com>
2019-03-21 23:29:59 +0530

    firstCommit

---
+++ index.js
@@ -0,0 +1,8 @@
+let multiplyMagic = (a=1,b=1)=>{
+  // start your code here.
+  
+  return a*b;
+
+};
+
+console.log(multiplyMagic())
---
+++ myScript.js
@@ -0,0 +1,15 @@
+var itemsPurchased = window.prompt("Entere the total items purchased")
+
+if(itemsPurchased>3){
+alert("you got a promo code");
+}
+
+else if(itemsPurchased==3){
+alert("Buy one more item to get a promo code");
+}
+
+else {
+alert("You are not eligible for a promocode");
+}
+
+console.log("Success on the Second commit")
